dbfef487c5673331f498a49091464f31a08909ce
LuisDonaldo2003 <l21930214@cdaltamirano.tecnm.mx>
2023-10-23 11:26:10 -0600

    Updated

--- database/migrations/2023_10_23_162009_create_users_table.php
+++ database/migrations/2023_10_23_162009_create_users_table.php
@@ -18,8 +18,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('paternal_last_name', 30);
-            $table->string('maternal_last_name', 30);
             $table->timestamps();
         });
     }
